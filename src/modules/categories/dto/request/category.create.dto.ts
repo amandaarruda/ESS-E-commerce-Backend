0e19d4873750f358c6a3cc8bde837df87974fc92
annecollier <apcm@cin.ufpe.br>
2024-06-22 23:35:50 -0300

    update

--- src/modules/categories/dto/request/category.create.dto.ts
+++ src/modules/categories/dto/request/category.create.dto.ts
@@ -1,12 +1,6 @@
 import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
 import { Transform, TransformFnParams } from 'class-transformer';
-import {
-  IsNotEmpty,
-  IsNumber,
-  IsOptional,
-  IsString,
-  MaxLength,
-} from 'class-validator';
+import { IsNotEmpty, IsOptional, IsString, MaxLength } from 'class-validator';
 
 export class CategoryCreateDto {
   @ApiProperty({
@@ -38,14 +32,4 @@ export class CategoryCreateDto {
       typeof value === 'string' && value?.trim(),
   )
   imageUrl: string;
-}
-
-export class CategoryUpdateDto extends CategoryCreateDto {
-  @ApiProperty({
-    example: '1',
-    description: 'Id da categoria',
-  })
-  @IsNotEmpty({ message: 'O campo de Id deve ser preenchido' })
-  @IsNumber()
-  id: number;
 }
