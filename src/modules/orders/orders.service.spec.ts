5141ae113d560cb22dcf334465363c4226b83b7c
Amanda Arruda <66084295+amandaarruda@users.noreply.github.com>
2024-06-24 13:23:40 -0300

    fix: removed unnecessary tests
    Removed unnecessary tests that only checked if controllers/services were defined.
    Used the .feature values ​​to populate the variables in the service test.

--- src/modules/orders/orders.service.spec.ts
+++ src/modules/orders/orders.service.spec.ts
@@ -2,6 +2,7 @@ import { Test, TestingModule } from '@nestjs/testing';
 
 import { PrismaService } from '../../database/prisma/prisma.service';
 import { EmailService } from '../email/email.service';
+import { EmailUtils } from '../email/email.utils';
 import { OrdersService } from './orders.service';
 
 describe('OrdersService', () => {
@@ -35,28 +36,24 @@ describe('OrdersService', () => {
     emailService = module.get<EmailService>(EmailService);
   });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
-  });
-
   describe('createOrder', () => {
-    it('deve criar um novo pedido e enviar email de confirmação', async () => {
-      const orderData = {
-        email: 'test@example.com',
-        code: 'order123',
-        price: 100.0,
-        userId: 1,
-        deliveryAddressId: null,
-      };
-      const createdOrder = {
-        id: 1,
-        ...orderData,
-        estimatedDelivery: new Date(),
-        status: 'PROCESSING',
-        createdAt: new Date(),
-        updatedAt: new Date(),
-      };
+    const orderData = {
+      email: 'test@example.com',
+      code: 'order123',
+      price: 100.0,
+      userId: 1,
+      deliveryAddressId: null,
+    };
+    const createdOrder = {
+      id: 1,
+      ...orderData,
+      estimatedDelivery: new Date(),
+      status: 'PROCESSING',
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    };
 
+    it('deve criar um novo pedido e enviar email de confirmação', async () => {
       (prismaService.order.create as jest.Mock).mockResolvedValue(createdOrder);
       (emailService.sendEmail as jest.Mock).mockResolvedValue(true);
 
@@ -72,31 +69,14 @@ describe('OrdersService', () => {
         },
       });
       expect(emailService.sendEmail).toHaveBeenCalledWith(
-        `<h1>Eba! Seu pedido foi confirmado. Agradecemos a preferência pelo seu pedido!</h1>
-         <p>Seu pedido com código order123 foi recebido e está sendo processado.</p>`,
+        EmailUtils.getOrderConfirmationEmail(orderData.code),
         'Confirmação de Pedido',
-        'test@example.com',
+        orderData.email,
       );
       expect(result).toEqual(createdOrder);
     });
 
     it('deve lidar com falha no envio do email', async () => {
-      const orderData = {
-        email: 'test@example.com',
-        code: 'order123',
-        price: 100.0,
-        userId: 1,
-        deliveryAddressId: null,
-      };
-      const createdOrder = {
-        id: 1,
-        ...orderData,
-        estimatedDelivery: new Date(),
-        status: 'PROCESSING',
-        createdAt: new Date(),
-        updatedAt: new Date(),
-      };
-
       (prismaService.order.create as jest.Mock).mockResolvedValue(createdOrder);
       (emailService.sendEmail as jest.Mock).mockRejectedValue(
         new Error('Falha ao enviar o email'),
@@ -116,10 +96,9 @@ describe('OrdersService', () => {
         },
       });
       expect(emailService.sendEmail).toHaveBeenCalledWith(
-        `<h1>Eba! Seu pedido foi confirmado. Agradecemos a preferência pelo seu pedido!</h1>
-         <p>Seu pedido com código order123 foi recebido e está sendo processado.</p>`,
+        EmailUtils.getOrderConfirmationEmail(orderData.code),
         'Confirmação de Pedido',
-        'test@example.com',
+        orderData.email,
       );
     });
   });
