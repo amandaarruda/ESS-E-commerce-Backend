21f0bb2d3af9b6689bdfd00cd733f3aaca8ad04a
Caio Verçosa <31866855+CaioRSV@users.noreply.github.com>
2024-06-25 12:40:53 -0300

    Update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,61 +4,6 @@ import {
   SnakeCaseNamingConvention,
 } from '@automapper/core';
 import { AutomapperModule } from '@automapper/nestjs';
-import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
-import { APP_FILTER, APP_GUARD } from '@nestjs/core';
-import { JwtModule } from '@nestjs/jwt';
-
-import { AuthModule } from './auth/auth.module';
-import { AtGuard } from './auth/guards';
-import { RolesGuard } from './auth/guards/roles.guard';
-import { PrismaModule } from './database/prisma/prisma.module';
-import { AllExceptionsFilter } from './middlewares/exception.filter';
-import { CartModule } from './modules/cart/cart.module';
-import { CategoriesModule } from './modules/categories/categories.module';
-import { EmailModule } from './modules/email/email.module';
-import { UserModule } from './modules/user/user.module';
-
-@Module({
-  providers: [
-    { provide: APP_GUARD, useClass: AtGuard },
-    { provide: APP_GUARD, useClass: RolesGuard },
-    {
-      provide: APP_FILTER,
-      useClass: AllExceptionsFilter,
-    },
-  ],
-  imports: [
-    EmailModule,
-    ConfigModule.forRoot(),
-    AutomapperModule.forRoot({
-      strategyInitializer: classes(),
-      namingConventions: {
-        source: new CamelCaseNamingConvention(),
-        destination: new SnakeCaseNamingConvention(),
-      },
-    }),
-    JwtModule.register({
-      secret: process.env.JWT_SECRET,
-    }),
-    AuthModule,
-    PrismaModule,
-    UserModule,
-    EmailModule,
-    CartModule,
-    CategoriesModule,
-  ],
-})
-export class AppModule implements NestModule {
-  configure(consumer: MiddlewareConsumer) {}
-}
-=======
-import { classes } from '@automapper/classes';
-import {
-  CamelCaseNamingConvention,
-  SnakeCaseNamingConvention,
-} from '@automapper/core';
-import { AutomapperModule } from '@automapper/nestjs';
 import { HttpModule } from '@nestjs/axios';
 import { Module, NestModule, MiddlewareConsumer } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
@@ -76,6 +21,8 @@ import { DeliveryModule } from './modules/delivery/delivery.module';
 import { EmailModule } from './modules/email/email.module';
 import { OrdersModule } from './modules/orders/orders.module';
 import { UserModule } from './modules/user/user.module';
+import { CartModule } from './modules/cart/cart.module';
+
 
 @Module({
   providers: [
@@ -111,6 +58,7 @@ import { UserModule } from './modules/user/user.module';
     CategoriesModule,
     OrdersModule,
     DeliveryModule,
+    CartModule,
   ],
 })
 export class AppModule implements NestModule {
