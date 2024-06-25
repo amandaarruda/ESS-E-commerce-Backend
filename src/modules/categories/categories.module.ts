6d406f63d6acd6de15927b740911691ccef631f4
Caio Verçosa <31866855+CaioRSV@users.noreply.github.com>
2024-06-25 12:42:02 -0300

    Update categories.module.ts

--- src/modules/categories/categories.module.ts
+++ src/modules/categories/categories.module.ts
@@ -12,18 +12,3 @@ import { CategoriesService } from './categories.service';
   exports: [CategoriesService, CategoriesRepository],
 })
 export class CategoriesModule {}
-=======
-import { Module } from '@nestjs/common';
-import { PrismaModule } from 'src/database/prisma/prisma.module';
-
-import { CategoriesController } from './categories.controller';
-import { CategoriesRepository } from './categories.repository';
-import { CategoriesService } from './categories.service';
-
-@Module({
-  controllers: [CategoriesController],
-  providers: [CategoriesService, CategoriesRepository],
-  imports: [PrismaModule],
-  exports: [CategoriesService, CategoriesRepository],
-})
-export class CategoriesModule {}
