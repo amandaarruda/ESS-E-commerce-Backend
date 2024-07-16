5b424cc6a8c4378ab739caa84ef780624e6b72b6
annecollier <apcm@cin.ufpe.br>
2024-07-16 16:12:18 -0300

    fix

--- src/app.module.ts
+++ src/app.module.ts
@@ -16,11 +16,11 @@ import { AtGuard } from './auth/guards';
 import { RolesGuard } from './auth/guards/roles.guard';
 import { PrismaModule } from './database/prisma/prisma.module';
 import { AllExceptionsFilter } from './middlewares/exception.filter';
+import { CartModule } from './modules/cart/cart.module';
 import { CategoriesModule } from './modules/categories/categories.module';
 import { DeliveryModule } from './modules/delivery/delivery.module';
 import { EmailModule } from './modules/email/email.module';
 import { OrdersModule } from './modules/orders/orders.module';
-import { ProductController } from './modules/product/product.controller';
 import { ProductModule } from './modules/product/product.module';
 import { UserModule } from './modules/user/user.module';
 
@@ -59,6 +59,7 @@ import { UserModule } from './modules/user/user.module';
     ProductModule,
     OrdersModule,
     DeliveryModule,
+    CartModule,
   ],
 })
 export class AppModule implements NestModule {
