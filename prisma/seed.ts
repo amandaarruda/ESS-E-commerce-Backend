f76cdce3aecf6a40801cb3058a90ba56334c6a7d
Caio Verçosa <31866855+CaioRSV@users.noreply.github.com>
2024-07-28 17:22:49 -0300

    fix: adapting Database Seed to Cart and OrderList specs

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -4,6 +4,9 @@ import * as bcrypt from 'bcrypt';
 export const ADMIN_EMAIL = 'admin@gmail.com';
 export const ADMIN_PASSWORD = 'admin';
 
+export const TEST_USER_EMAIL = 'teste@gmail.com';
+export const TEST_USER_PASSWORD = '@Teste123';
+
 const prisma = new PrismaClient();
 
 const seedUser = async (prisma: PrismaClient): Promise<void> => {
@@ -36,7 +39,130 @@ const seedUser = async (prisma: PrismaClient): Promise<void> => {
         role: RoleEnum.ADMIN,
       },
     });
+
+    const AdminUser = await prisma.user.findFirst({
+      where: {
+        name: 'Admin User',
+      },
+    });
+
+    await prisma.cart.create({
+      data: {
+        userId: AdminUser.id,
+        locked: false,
+      },
+    });
   }
+
+  // Utils Carrinho & Histórico de Pedidos
+  console.log('Seeding Test User');
+
+  await prisma.user.create({
+    data: {
+      email: TEST_USER_EMAIL,
+      name: 'Teste',
+      Media: {
+        create: {
+          url: 'https://example.com/image.jpg',
+        },
+      },
+      password: await bcrypt.hash(TEST_USER_PASSWORD, 10),
+      status: StatusEnum.ACTIVE,
+      recoveryPasswordToken: null,
+      refreshToken: null,
+      deletedAt: null,
+      role: RoleEnum.CUSTOMER,
+    },
+  });
+
+  const TestUser = await prisma.user.findFirst({
+    where: {
+      name: 'Teste',
+    },
+  });
+
+  console.log('Seeding Tênis category');
+
+  await prisma.category.create({
+    data: {
+      name: 'Tênis',
+    },
+  });
+
+  const TenisCategory = await prisma.category.findUnique({
+    where: {
+      name: 'Tênis',
+    },
+  });
+
+  console.log('Seeding Produto A product');
+
+  await prisma.product.create({
+    data: {
+      name: 'Produto A',
+      price: 50.5,
+      stock: 50,
+      categoryId: TenisCategory.id,
+      description: 'Produto A description',
+    },
+  });
+
+  const newProduct = await prisma.product.findFirst({
+    where: {
+      name: 'Produto A',
+    },
+  });
+
+  console.log('Seeding Test User cart');
+
+  await prisma.cart.create({
+    data: {
+      userId: TestUser.id,
+      locked: false,
+    },
+  });
+
+  const newCart = await prisma.cart.findUnique({
+    where: {
+      userId: TestUser.id,
+    },
+  });
+
+  await prisma.cartProduct.create({
+    data: {
+      cartId: newCart.id,
+      productId: newProduct.id,
+      userId: TestUser.id,
+      quantity: 1,
+    },
+  });
+
+  console.log('Seeding Test User sample order');
+
+  await prisma.order.create({
+    data: {
+      code: '#testOrder#123',
+      price: 50.5,
+      userId: TestUser.id,
+      estimatedDelivery: new Date('2024-08-01T00:00:00Z'), // Data de entrega estimada
+    },
+  });
+
+  const newOrder = await prisma.order.findUnique({
+    where: {
+      code: '#testOrder#123',
+    },
+  });
+
+  await prisma.orderProduct.create({
+    data: {
+      orderId: newOrder.id,
+      productId: newProduct.id,
+      quantity: 1,
+    },
+  });
+
+  //
 };
 
 (async () => {
