425d1b86d3e92b138fe6fa9138ba2b51995a2291
BrenoRev <silvabreno462@gmail.com>
2024-07-28 17:03:04 -0300

    chore(logs): removing console logs

--- src/utils/templates/processors/recover-password-processor.ts
+++ src/utils/templates/processors/recover-password-processor.ts
@@ -9,13 +9,9 @@ export const recoverTemplateDataBind = (
   },
 ): string => {
   let template = baseProcessor(templateBody);
-  console.log(template);
   template = template.replace('{{CSS}}', recoverPasswordCss);
-  console.log('2', template);
   template = template.replace(/{{userName}}/g, data.name);
-  console.log('3', template);
   template = template.replace('{{link}}', data.link);
-  console.log('4', template);
 
   return template;
 };
